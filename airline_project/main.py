59cc4f642b4806e937dcb29a63bfbef795a185cc
Anant Sakhare <70131870+senhorinfinito@users.noreply.github.com>
2021-07-04 13:37:06 +0530

    Delete airline_project directory

--- airline_project/main.py
+++
@@ -1,8 +0,0 @@
-import scrapy_tripadvis as st
-import pandas as pd
-
-path = r"C:\Users\Anant Sakhare\Desktop\scrapper\airline_project\chromedriver.exe"
-
-df = st.get_jobs('data scientist',1000, False, path, 15)
-
-df.to_csv('glassdoor_jobs.csv', index = False)
